ac235b53397c89c5adc5613ae5b0145762cc2b1e
DiegoVivas13 <62918291+DiegoVivas13@users.noreply.github.com>
2020-03-31 21:29:09 -0500

    Mi primera modificacion

---
+++ main.py
@@ -0,0 +1,17 @@
+
+a=float(input("digite un numero: "))
+c=float(input("digite un numero: "))
+product= a*c
+doble= 2*a
+print("el producto de los numeros es", product)
+print("el doble del primer numero es", doble)
+
+b=float(input("digite un numero: "))
+b=abs(b)
+b=b*b
+print("el cuadrado de el numero b es", b)
+
+d=float(input("digite un numero: "))
+d=abs(d)
+d= d**(1/2)
+print("la raiz del numero d es", d)
