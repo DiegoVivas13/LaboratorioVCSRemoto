4177f4e512d6b88b29d114c27c00cff97bffbf3c
DiegoVivas13 <62918291+DiegoVivas13@users.noreply.github.com>
2020-03-31 22:18:43 -0500

    faltaba cerrar el primer if y darle respuesta

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 
-a=float(input("digite un numero: "))
-c=float(input("digite un numero: "))
-product= a*c
-doble= 2*a
-print("el producto de los numeros es", product)
-print("el doble del primer numero es", doble)
-
-b=float(input("digite un numero: "))
-b=abs(b)
-b=b*b
-print("el cuadrado de el numero b es", b)
-
-d=float(input("digite un numero: "))
-d=abs(d)
-d= d**(1/2)
-print("la raiz del numero d es", d)
+a=float(input("digite el numero a: "))
+b=float(input("digite el numero b: "))
+c=float(input("digite el numero c: "))
+if a!=0:
+    d= b**b-4*a*c
+    if d>0:
+        x1= (-b+d**(1/2))/(2*a)
+        x2= (-b-d**(1/2))/(2*a)
+        print("las respuestas son", x1, "y", x2)
+    elif d==0:
+        x= (-b)/(2*a)
+        print("x1 y x2 son iguales y corresponden a ", x)
+    elif d<0:
+        print("no existe solucion a la ecuacion dentro de el dominio de los numeros reales")
+else:
+    print("la ecuacion no es una funcion cuadratica")
